b86e8794d3cf37dc24bd0b3ef8505ab67ef4621d
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:54:58 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "automation_connection_service_principals" {
+  description = "All automation_connection_service_principal resources"
+  value       = azurerm_automation_connection_service_principal.automation_connection_service_principals
+}
+
