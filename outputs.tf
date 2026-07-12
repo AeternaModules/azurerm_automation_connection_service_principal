23ff9ce3c0c96b240957be1fab3da4008027465b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:58 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "automation_connection_service_principals" {
-  description = "All automation_connection_service_principal resources"
-  value       = azurerm_automation_connection_service_principal.automation_connection_service_principals
-}
 output "automation_connection_service_principals_application_id" {
-  description = "List of application_id values across all automation_connection_service_principals"
-  value       = [for k, v in azurerm_automation_connection_service_principal.automation_connection_service_principals : v.application_id]
+  description = "Map of application_id values across all automation_connection_service_principals, keyed the same as var.automation_connection_service_principals"
+  value       = { for k, v in azurerm_automation_connection_service_principal.automation_connection_service_principals : k => v.application_id }
 }
 output "automation_connection_service_principals_automation_account_name" {
-  description = "List of automation_account_name values across all automation_connection_service_principals"
-  value       = [for k, v in azurerm_automation_connection_service_principal.automation_connection_service_principals : v.automation_account_name]
+  description = "Map of automation_account_name values across all automation_connection_service_principals, keyed the same as var.automation_connection_service_principals"
+  value       = { for k, v in azurerm_automation_connection_service_principal.automation_connection_service_principals : k => v.automation_account_name }
 }
 output "automation_connection_service_principals_certificate_thumbprint" {
-  description = "List of certificate_thumbprint values across all automation_connection_service_principals"
-  value       = [for k, v in azurerm_automation_connection_service_principal.automation_connection_service_principals : v.certificate_thumbprint]
+  description = "Map of certificate_thumbprint values across all automation_connection_service_principals, keyed the same as var.automation_connection_service_principals"
+  value       = { for k, v in azurerm_automation_connection_service_principal.automation_connection_service_principals : k => v.certificate_thumbprint }
 }
 output "automation_connection_service_principals_description" {
-  description = "List of description values across all automation_connection_service_principals"
-  value       = [for k, v in azurerm_automation_connection_service_principal.automation_connection_service_principals : v.description]
+  description = "Map of description values across all automation_connection_service_principals, keyed the same as var.automation_connection_service_principals"
+  value       = { for k, v in azurerm_automation_connection_service_principal.automation_connection_service_principals : k => v.description }
 }
 output "automation_connection_service_principals_name" {
-  description = "List of name values across all automation_connection_service_principals"
-  value       = [for k, v in azurerm_automation_connection_service_principal.automation_connection_service_principals : v.name]
+  description = "Map of name values across all automation_connection_service_principals, keyed the same as var.automation_connection_service_principals"
+  value       = { for k, v in azurerm_automation_connection_service_principal.automation_connection_service_principals : k => v.name }
 }
 output "automation_connection_service_principals_resource_group_name" {
-  description = "List of resource_group_name values across all automation_connection_service_principals"
-  value       = [for k, v in azurerm_automation_connection_service_principal.automation_connection_service_principals : v.resource_group_name]
+  description = "Map of resource_group_name values across all automation_connection_service_principals, keyed the same as var.automation_connection_service_principals"
+  value       = { for k, v in azurerm_automation_connection_service_principal.automation_connection_service_principals : k => v.resource_group_name }
 }
 output "automation_connection_service_principals_subscription_id" {
-  description = "List of subscription_id values across all automation_connection_service_principals"
-  value       = [for k, v in azurerm_automation_connection_service_principal.automation_connection_service_principals : v.subscription_id]
+  description = "Map of subscription_id values across all automation_connection_service_principals, keyed the same as var.automation_connection_service_principals"
+  value       = { for k, v in azurerm_automation_connection_service_principal.automation_connection_service_principals : k => v.subscription_id }
 }
 output "automation_connection_service_principals_tenant_id" {
-  description = "List of tenant_id values across all automation_connection_service_principals"
-  value       = [for k, v in azurerm_automation_connection_service_principal.automation_connection_service_principals : v.tenant_id]
+  description = "Map of tenant_id values across all automation_connection_service_principals, keyed the same as var.automation_connection_service_principals"
+  value       = { for k, v in azurerm_automation_connection_service_principal.automation_connection_service_principals : k => v.tenant_id }
 }
 
